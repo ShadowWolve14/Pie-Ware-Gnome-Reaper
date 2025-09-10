8ea10b946a138e608c8f51151d923e7bb4d412db
Will <Willi.Becker@stud.srh-university.de>
2025-09-10 19:59:52 +0200

    Bomben Bug fix

--- src/game/PlayerBaseClass.cpp
+++ src/game/PlayerBaseClass.cpp
@@ -350,7 +350,7 @@ void Player_Base_Class::PickUpItem(ItemBase* item_to_pick_up)
 
 void Player_Base_Class::Use_Item()
 {
-    if (IsKeyPressed(game::Config::key_Use_Item) && HasItem() && held_item->GetType() != ItemType::KEY && !is_buffed)
+    if (IsKeyPressed(game::Config::key_Use_Item) && HasItem() && held_item->GetType() != ItemType::KEY && !is_buffed && item_removal_timer <= 0.0f)
     {
         held_item->Activate(this);
     }
