3ee4f41c65c4c3eaa44f9d38bab4b42a560ef5fe
Will <Willi.Becker@stud.srh-university.de>
2025-09-16 23:45:31 +0200

    Zusätzliche Heiltränke

--- src/game/HealthPotion.cpp
+++ src/game/HealthPotion.cpp
@@ -4,19 +4,21 @@
 
 #include "HealthPotion.h"
 #include "PlayerBaseClass.h"
+#include "Store.h"
 #include "../Config.h.in"
 
 HealthPotion::HealthPotion(Vector2 position, ItemType type, const char* anim_path, int p_heal_amount)
     : ItemBase(position, type, anim_path, true,
                game::Config::health_Potion_Anim_Size, game::Config::health_Potion_Frame_Count, game::Config::health_Potion_Anim_Speed),
-      heal_amount(p_heal_amount) { }
+      heal_amount(p_heal_amount)  { }
 
 HealthPotion::HealthPotion(Vector2 position)
-    : HealthPotion(position, ItemType::HEALTH_POTION, game::Config::kHealthPotionAnim, game::Config::health_Potion_Heal_Amount) { }
+: HealthPotion(position, ItemType::HEALTH_POTION, game::Config::kHealthPotionAnim, game::Config::health_Potion_Heal_Amount) {}
 
 void HealthPotion::Activate(Player_Base_Class* player)
 {
     PlaySound(us);
+    SetSoundVolume(us, game::core::Store::volume);
     player->Take_Damage(-this->heal_amount);
     player->RemoveHeldItem();
 }
